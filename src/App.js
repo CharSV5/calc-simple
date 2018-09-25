4a5566cb41473ec4d2c9f1ac182157d8ba7e1059
charlene bastians <charlenebastians@hotmail.com>
2018-09-25 11:15:10 +0100

    first two number buttons being recorded as input 1 and input 2

--- src/App.js
+++ src/App.js
@@ -11,107 +11,117 @@ class App extends Component {
       inputValue2: 0,
       currentMaths: '',
       answer: 0
-    }  
-    this.handleChange = this.handleChange.bind(this);
+    }
     this.handleSubmit = this.handleSubmit.bind(this);
     this.handleClick = this.handleClick.bind(this);
     this.plus = this.plus.bind(this);
     this.minus = this.minus.bind(this);
     this.divide = this.divide.bind(this);
     this.multiply = this.multiply.bind(this);
+    this.decideMaths = this.decideMaths.bind(this);
 
-}
+  }
 
 
 
-handleChange (event) {
-  this.setState({
-    [event.target.name]: event.target.value
-  })
-}
+  handleChange(event) {
+    this.setState({
+      [event.target.name]: event.target.value
+    })
+  }
 
-plus(inputValue1, inputValue2) {
+  plus(inputValue1, inputValue2) {
     this.setState({
       answer: (parseInt(inputValue1) + parseInt(inputValue2))
     })
-}
-
-minus(inputValue1, inputValue2) {
-  this.setState({
-    answer: (parseInt(inputValue1) - parseInt(inputValue2))
-  })
-}
-
-divide(inputValue1, inputValue2) {
-  this.setState({
-    answer: (parseInt(inputValue1) / parseInt(inputValue2))
-  })
-}
-
-multiply(inputValue1, inputValue2) {
-  this.setState({
-    answer: (parseInt(inputValue1) * parseInt(inputValue2))
-  })
-}
-
-decideMaths(currentMathsFunc, inputValue1, inputValue2) {
-  return currentMathsFunc(inputValue1, inputValue2)
-}
-
-handleSubmit(event) {
-  event.preventDefault();
-  const inputValue1 = this.state.inputValue1
-  const inputValue2 = this.state.inputValue2
-  let currentMathsFunc = ''
-
-  switch(this.state.currentMaths) {
-    case '+':
-    currentMathsFunc = this.plus
-    break;
-
-    case '-':
-    currentMathsFunc = this.minus
-    break;
-
-    case '/':
-    currentMathsFunc = this.divide
-    break;
-
-    case '*':
-    currentMathsFunc = this.multiply
-    break;
   }
-  this.decideMaths(currentMathsFunc, inputValue1, inputValue2)
-}
+
+  minus(inputValue1, inputValue2) {
+    this.setState({
+      answer: (parseInt(inputValue1) - parseInt(inputValue2))
+    })
+  }
+
+  divide(inputValue1, inputValue2) {
+    this.setState({
+      answer: (parseInt(inputValue1) / parseInt(inputValue2))
+    })
+  }
+
+  multiply(inputValue1, inputValue2) {
+    this.setState({
+      answer: (parseInt(inputValue1) * parseInt(inputValue2))
+    })
+  }
+
+  decideMaths(currentMathsFunc, inputValue1, inputValue2) {
+    return currentMathsFunc(inputValue1, inputValue2)
+  }
+
+  handleSubmit(event) {
+    console.log('submit!')
+    event.preventDefault();
+    const inputValue1 = this.state.inputValue1
+    const inputValue2 = this.state.inputValue2
+    let currentMathsFunc = ''
+
+    switch (this.state.currentMaths) {
+      case '+':
+        
+        currentMathsFunc = this.plus
+        break;
+
+      case '-':
+        currentMathsFunc = this.minus
+        break;
+
+      case '/':
+        currentMathsFunc = this.divide
+        break;
+
+      case '*':
+        currentMathsFunc = this.multiply
+        break;
+    }
+    this.decideMaths(currentMathsFunc, inputValue1, inputValue2)
+  }
 
 
 
-handleClick(maths) {
-  this.setState({
-    currentMaths: maths
-  })
-}
+  handleClick(maths) {
+    this.setState({
+      currentMaths: maths
+    })
+  }
+
+  handleNum(num) {
+    if(this.state.inputValue1 === 0) {
+      this.setState({
+        inputValue1: num
+      })
+    } else {
+      this.setState({
+        inputValue2: num
+      })
+    }
+  }
+  
 
 
   render() {
     return (
       <div className="App">
-      <form onSubmit={this.handleSubmit}>
-       <label>
-          Input 1
-          <input name="inputValue1" type="text" value={this.state.inputValue1} onChange={this.handleChange} />
-        </label>
-        <button onClick={() => this.handleClick('+')}>+</button>
-        <button onClick={() => this.handleClick('-')}>-</button>
-        <button onClick={() => this.handleClick('/')}>/</button>
-        <button onClick={() => this.handleClick('*')}>*</button>
-        <label>
-          Input 2
-          <input name="inputValue2" type="text" value={this.state.inputValue2} onChange={this.handleChange} />
-        </label>
-        <input type="submit" value="=" />
+        
+          <button onClick={() => this.handleNum(1)}>1</button>
+          <button onClick={() => this.handleNum(2)}>2</button>
+          <button onClick={() => this.handleClick('+')}>+</button>
+          <button onClick={() => this.handleClick('-')}>-</button>
+          <button onClick={() => this.handleClick('/')}>/</button>
+          <button onClick={() => this.handleClick('*')}>*</button>
+          <form onSubmit={this.handleSubmit}>
+          <input type="submit" value="=" />
         </form>
-       {this.state.answer}
+        {this.state.answer}
       </div>
     );
   }
